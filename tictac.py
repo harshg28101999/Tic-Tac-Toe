409029635232bc507f7f0c98725cd46cf0eb9693
Harsh Gupta <43788815+harshg28101999@users.noreply.github.com>
2020-06-29 22:24:15 -0400

    Final Tic Tac project

--- tictac.py
+++ tictac.py
@@ -19,22 +19,6 @@ def display_board():
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-def play_game():
-    # Display the initial board.
-    display_board()
-
-    while game_still_going: 
-        handle_turn(current_player)
-
-        check_if_game_over()
-        flip_player()
-    
-    # Game has ended
-    if winner == "X" or winner == "O":
-        print(winner + " won.")
-    elif winner == None:
-        print("Tie")
-
 def handle_turn(player):
 
     print (player + "'s turn")
@@ -57,9 +41,15 @@ def handle_turn(player):
     board[position] = player
     display_board()
 
-def check_if_game_over():
-    check_for_winner()
-    check_if_tie()
+def flip_player():
+    global current_player
+
+    if current_player == "X":
+        current_player = "O"
+
+    elif current_player == "O":
+        current_player = "X" 
+    return
 
 def check_for_winner():
 
@@ -143,14 +133,24 @@ def check_if_tie():
         game_still_going = False
     return
 
-def flip_player():
-    global current_player
+def check_if_game_over():
+    check_for_winner()
+    check_if_tie()
+    
+def play_game():
+    # Display the initial board.
+    display_board()
 
-    if current_player == "X":
-        current_player = "O"
+    while game_still_going: 
+        handle_turn(current_player)
 
-    elif current_player == "O":
-        current_player = "X" 
-    return
+        check_if_game_over()
+        flip_player()
+    
+    # Game has ended
+    if winner == "X" or winner == "O":
+        print(winner + " won.")
+    elif winner == None:
+        print("Tie")
 
 play_game()
