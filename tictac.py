db9c902cace4a246e54f7df3e7be5b75945b834a
Harsh Gupta <43788815+harshg28101999@users.noreply.github.com>
2020-08-09 20:47:54 +0400

    Update tictac.py

--- tictac.py
+++ tictac.py
@@ -28,7 +28,7 @@ def handle_turn(player):
     while not valid:
 
         while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-            position = input("Choose a position from 1-9: ")
+            position = input("Choose a valid position from 1-9: ")
     
 
         position = int(position) - 1
